5d24cbd4b39d3ec870447068a2f961e2c2652ab8
Sofya Govorova <govorova932@gmail.com>
2022-10-07 15:44:21 +0300

    code for ordercontroller

--- src/main/java/com/example/pizzeria/controller/OrderController.java
+++ src/main/java/com/example/pizzeria/controller/OrderController.java
@@ -29,24 +29,48 @@ public class OrderController {
     private GoodsService goodsService;
     @PostMapping
     public String createOrder(CreateOrderDTO orderDTO, @AuthenticationPrincipal UserEntity user, RedirectAttributes attrs) {
-        return  null;
-
+        try {
+            orderService.createOrder(orderDTO, user);
+            attrs.addFlashAttribute("message", "Заказ сделан");
+            return "redirect:/home";
+        } catch (Exception e) {
+            attrs.addFlashAttribute("errorTitle", "Ошибка при создании");
+            attrs.addFlashAttribute("errorMessage", e.getMessage());
+            return "redirect:/error";
+        }
     }
 
     @GetMapping
     public String getOrders(Model model, @AuthenticationPrincipal UserEntity user) {
-        return  null;
-
+        List<OrderEntity> orders = orderService.getOrders(user);
+        Long totalCost = orderService.getSummaryCost(user);
+        model.addAttribute("orders", orders);
+        model.addAttribute("cost", totalCost);
+        return "orders";
     }
 
     @GetMapping("/add")
     public String getCreateOrdersPage(Model model) {
-        return  null;
-
+        CreateOrderDTO orderDto = new CreateOrderDTO();
+        FilterGoodsDTO filterDto = new FilterGoodsDTO();
+        List<GoodsEntity> allGoods = goodsService.getAllGoods(filterDto);
+        val filterDtoFromAttr = model.getAttribute("filterDto");
+        val orderDtoFromAttr = model.getAttribute("orderDto");
+        val goodsFromAttr = model.getAttribute("goods");
+        CreateOrderDTO orderDtoCasted = orderDtoFromAttr == null ? orderDto : (CreateOrderDTO) orderDtoFromAttr;
+        val checkedGoods = goodsService.getAllByIds(orderDtoCasted.getGoodsIds());
+        model.addAttribute("goods", goodsFromAttr == null ? Goods.toModel(allGoods) : goodsFromAttr);
+        model.addAttribute("checkedGoods", checkedGoods);
+        model.addAttribute("filterDto", filterDtoFromAttr == null ? filterDto : filterDtoFromAttr);
+        model.addAttribute("orderDto", orderDtoFromAttr == null ? orderDto : orderDtoFromAttr);
+        return "addOrder";
     }
     @PostMapping("/add")
     public String updateCreateOrderPage(RedirectAttributes attrs, FilterGoodsDTO dto, CreateOrderDTO orderDto) {
-        return  null;
-
+        List<GoodsEntity> allGoods = goodsService.getAllGoods(dto);
+        attrs.addFlashAttribute("goods", Goods.toModel(allGoods));
+        attrs.addFlashAttribute("filterDto", dto);
+        attrs.addFlashAttribute("orderDto", orderDto);
+        return "redirect:/order/add";
     }
 }
